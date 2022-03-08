8f0499852b6ddc53b473db0d7e99934adda35429
ink-dot <101213481+ink-dot@users.noreply.github.com>
2022-03-08 17:00:12 -0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -19,36 +19,13 @@ df_data_country = df_data_country.apply(pd.to_numeric, errors='coerce')
 country_stats = pd.DataFrame({'country': countries, 'mean': df_data_country.mean(axis=1),
                        'std': df_data_country.std(axis=1)})
 
-#render results
-fig, ax = plt.subplots(figsize=(14, 6), dpi = 50)
-ax.bar(countries, country_stats['mean'], yerr=country_stats['std'], capsize = 3)
-ax.set_axisbelow(True)  #ensure the grid is under the graph elements
-ax.margins(x=0.01) #set up the margin of graph
-ax.grid(alpha = 0.3) #show the grid line
-ax.set_xlabel('country')
-ax.set_ylabel('emissions')
-ax.set_title('The mean and std of emissions of countries')
+#heatmap
+fig, ax = plt.subplots(figsize=(20, 10))
+ax = sns.heatmap(df_data_country.T, linewidths=.5, cmap='flare_r')
+
+ax.set_xlabel('Country')
+ylabel = ax.set_ylabel('Year')
 xaxis = plt.xticks(rotation=90, ha='center', fontsize=8)
-yaxis = plt.yticks(fontsize=8)
-
-st.pyplot(fig)
-
-st.subheader("altair chart")
-
-chart_data = data.drop(columns=['Non-OECD Economies'])
-chart_data = pd.melt(chart_data, id_vars=['Country\year'], var_name='year')
-chart_data['value'] = chart_data['value'].apply(pd.to_numeric, errors='coerce')
-chart_data.rename(columns={"Country\year": "country", "value":"emission"}, inplace = True)
-chart_data
-
-#render using altair
-heatmap = alt.Chart(chart_data).mark_rect().encode(
-    x=alt.X('country:N', title = 'country'),
-    y=alt.Y('year:O', title = 'year'),
-    color='emission:Q',
-    tooltip=['country', 'year', 'emission']
-)
-
-st.altair_chart(heatmap, use_container_width = True)
+title = ax.set_title('Heatmap of Emissions of Countries over Years')
 
 
