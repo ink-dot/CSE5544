db96ac4944ecbb715693f8d2dedfb7a180d41d46
ink-dot <101213481+ink-dot@users.noreply.github.com>
2022-03-08 20:07:02 -0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -19,7 +19,7 @@ st.markdown("##### Link to Github code: https://github.com/ink-dot/CSE5544/edit/
 
 data = pd.read_csv("https://raw.githubusercontent.com/ink-dot/CSE5544/main/CSE5544.Lab1.ClimateData%20-%20Sheet1.csv")
 data = data.drop(columns=['Non-OECD Economies'])
-
+data.drop(index='OECD - Total', inplace=True)
 
 # Beginning of P1
 st.markdown("### P1: Honest/Ethical/Truthful")
