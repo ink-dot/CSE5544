ba4200951c8eff9dc0d8c1511968fb07d4f0133d
ink-dot <101213481+ink-dot@users.noreply.github.com>
2022-03-08 19:04:47 -0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -15,27 +15,13 @@ import altair as alt
 st.title("CSE 5544 Lab 3")
 st.markdown("### nekic.4")
 
-data = pd.read_csv("https://raw.githubusercontent.com/ink-dot/CSE5544/main/CSE5544.Lab1.ClimateData%20-%20Sheet1.csv")
-
-# Clean the data
-countries = data['Country\\year']
-df_data_country = data.iloc[:,2:]
-df_data_country = df_data_country.apply(pd.to_numeric, errors='coerce')
-country_stats = pd.DataFrame({'country': countries, 'mean': df_data_country.mean(axis=1),
-                       'std': df_data_country.std(axis=1)})
-# Render
-chart_data = data.drop(columns=['Non-OECD Economies'])
-chart_data = pd.melt(chart_data, id_vars=['Country\year'], var_name='year')
-chart_data['value'] = chart_data['value'].apply(pd.to_numeric, errors='coerce')
-chart_data.rename(columns={"Country\year": "country", "value":"emission"}, inplace = True)
-
-# Heatmap P1
-heatmap = alt.Chart(chart_data).mark_rect().encode(
-    x=alt.X('country:N', title = 'country'),
-    y=alt.Y('year:O', title = 'year'),
-    color='emission:Q',
-    tooltip=['country', 'year', 'emission']
-)
+df_data = pd.read_csv("https://raw.githubusercontent.com/ink-dot/CSE5544/main/CSE5544.Lab1.ClimateData%20-%20Sheet1.csv")
+df_data.columns = df_data.iloc[0]
+df_data.drop(df_data.index[0], inplace=True)
+df_data.set_index('Country\year', inplace=True)
+df_data = df_data.drop(columns=['Non-OECD Economies'])
+df_data.drop(index='OECD - Total', inplace=True)
+df_data.reset_index(inplace=True)
 
 st.header("P1: Honest/Ethical/Truthful")
-st.altair_chart(heatmap, use_container_width = True)
+
