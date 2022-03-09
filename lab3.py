a1e162e3159d4aa3120ee1217566442e7e80d256
ink-dot <101213481+ink-dot@users.noreply.github.com>
2022-03-08 20:06:02 -0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -18,6 +18,10 @@ st.markdown("##### nekic.4")
 st.markdown("##### Link to Github code: https://github.com/ink-dot/CSE5544/edit/main/lab3.py")
 
 data = pd.read_csv("https://raw.githubusercontent.com/ink-dot/CSE5544/main/CSE5544.Lab1.ClimateData%20-%20Sheet1.csv")
+data = data.drop(columns=['Non-OECD Economies'])
+data = pd.melt(chart_data, id_vars=['Country\year'], var_name='year')
+data['value'] = chart_data['value'].apply(pd.to_numeric, errors='coerce')
+data.rename(columns={"Country\year": "country", "value":"emission"}, inplace = True)
 
 # Beginning of P1
 st.markdown("### P1: Honest/Ethical/Truthful")
