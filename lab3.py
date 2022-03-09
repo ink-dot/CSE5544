5cd4b09fedede89edec1d7eb74cc9c8926db622a
ink-dot <101213481+ink-dot@users.noreply.github.com>
2022-03-08 19:19:19 -0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -15,11 +15,8 @@ import altair as alt
 st.title("CSE 5544 Lab 3")
 st.markdown("### nekic.4")
 
-df_data = pd.read_csv("https://raw.githubusercontent.com/ink-dot/CSE5544/main/CSE5544.Lab1.ClimateData%20-%20Sheet1.csv")
-df_data.columns = df_data.iloc[0]
-df_data.drop(df_data.index[0], inplace=True)
+data = pd.read_csv("https://raw.githubusercontent.com/ink-dot/CSE5544/main/CSE5544.Lab1.ClimateData%20-%20Sheet1.csv")
 
-df_data = df_data.drop(columns=['Non-OECD Economies'])
 
 st.header("P1: Honest/Ethical/Truthful")
 
