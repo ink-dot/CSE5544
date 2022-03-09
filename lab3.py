6db20f25377b7db209c4bbb1380a939ecd711d00
ink-dot <101213481+ink-dot@users.noreply.github.com>
2022-03-08 19:20:24 -0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -18,5 +18,16 @@ st.markdown("### nekic.4")
 data = pd.read_csv("https://raw.githubusercontent.com/ink-dot/CSE5544/main/CSE5544.Lab1.ClimateData%20-%20Sheet1.csv")
 
 
+df_data1 = data.apply(pd.to_numeric, errors='coerce')
+
+fig, ax = plt.subplots(figsize=(20, 10))
+ax = sns.heatmap(df_data1.T, linewidths=.5, cmap='rainbow', cbar_kws={'label': 'Emissions in Tons of C02'})
+
+ax.set_xlabel('Country')
+ylabel = ax.set_ylabel('Year')
+xaxis = plt.xticks(rotation=90, ha='center', fontsize=8)
+title = ax.set_title('Heatmap of Emissions of Countries over Years')
+
+
 st.header("P1: Honest/Ethical/Truthful")
 
