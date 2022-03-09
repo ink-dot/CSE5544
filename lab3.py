2361497d4bf94e6b267b0837ff38283a1d160566
ink-dot <101213481+ink-dot@users.noreply.github.com>
2022-03-08 19:06:22 -0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -18,10 +18,8 @@ st.markdown("### nekic.4")
 df_data = pd.read_csv("https://raw.githubusercontent.com/ink-dot/CSE5544/main/CSE5544.Lab1.ClimateData%20-%20Sheet1.csv")
 df_data.columns = df_data.iloc[0]
 df_data.drop(df_data.index[0], inplace=True)
-df_data.set_index('Country\year', inplace=True)
+
 df_data = df_data.drop(columns=['Non-OECD Economies'])
-df_data.drop(index='OECD - Total', inplace=True)
-df_data.reset_index(inplace=True)
 
 st.header("P1: Honest/Ethical/Truthful")
 
